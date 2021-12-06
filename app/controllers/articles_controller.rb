51f628d183e0590189fefa452ff653b8e9f42505
yocosaka <sakayoco55@gmail.com>
2021-12-06 15:53:13 +0900

    Change the json format using json serializer and write tests for requests of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
   def index
     articles = Article.all
-    render json: articles, status: :ok
+    render json: serializer.new(articles), status: :ok
   end
 
   def show
     id = params[:id]
     render json: { id: id }
+  end
+
+  def serializer
+    ArticleSerializer
   end
 end
--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,4 +1,5 @@
 class ArticleSerializer
   include JSONAPI::Serializer
+  # set_type :some_prefer_name
   attributes :title, :content, :slug
 end

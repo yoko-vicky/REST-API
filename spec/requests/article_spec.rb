8583361c794097122a949351d934076f2f2498a8
yocosaka <sakayoco55@gmail.com>
2021-12-07 15:07:30 +0900

    Add tests of the order of articles

--- spec/requests/article_spec.rb
+++ spec/requests/article_spec.rb
@@ -22,5 +22,14 @@ RSpec.describe ArticlesController do
                                             })
       end
     end
+
+    it 'returns articles in the latest order' do
+      recent_article = create(:article)
+      older_article = create(:article, created_at: 1.hour.ago)
+
+      get '/articles'
+      ids = json_data.map { |item| item[:id].to_i }
+      expect(ids).to eq([recent_article.id, older_article.id])
+    end
   end
 end

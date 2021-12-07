e009d47005de827f1c179965aaa95ed385da5a4f
yocosaka <sakayoco55@gmail.com>
2021-12-07 14:59:53 +0900

    Fix spec tests of json response data one by one

--- spec/requests/article_spec.rb
+++ spec/requests/article_spec.rb
@@ -10,19 +10,17 @@ RSpec.describe ArticlesController do
     it 'returns a proper JSON' do
       article = create :article
       get '/articles'
-      expect(json_data).to eq(
-        [
-          {
-            id: article.id.to_s,
-            type: 'article',
-            attributes: {
-              title: article.title,
-              content: article.content,
-              slug: article.slug
-            }
-          }
-        ]
-      )
+      expect(json_data.length).to eq(1)
+      expected = json_data[0]
+      aggregate_failures do
+        expect(expected[:id]).to eq(article.id.to_s)
+        expect(expected[:type]).to eq('article')
+        expect(expected[:attributes]).to eq({
+                                              title: article.title,
+                                              content: article.content,
+                                              slug: article.slug
+                                            })
+      end
     end
   end
 end

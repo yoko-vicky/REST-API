136482399504991c6251657ca005ead2f3983e0f
yocosaka <sakayoco55@gmail.com>
2021-12-09 17:30:46 +0900

    Add error handling in Show action using rescue

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,8 @@ class ArticlesController < ApplicationController
   def show
     article = Article.find(params[:id])
     render json: serializer.new(article)
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { message: e.message, detail: 'Here will be nicely formatted response' }
   end
 
   def serializer

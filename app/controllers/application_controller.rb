aba2b5552f021171d3f9669f2b2bb3c04e1efa95
yocosaka <sakayoco55@gmail.com>
2021-12-09 17:36:27 +0900

    Setup json api errors handler gem to handle all possible errors in controllers automatically and return 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  # Setup of json api errors handler gem which will automatically catch every errors that may happen
+  # and return 404 as response
+  include JsonapiErrorsHandler
+  ErrorMapper.map_errors!({
+                            'ActiveRecord::RecordNotFound' => 'JsonapiErrorsHandler::Errors::NotFound'
+                          })
+  rescue_from ::StandardError, with: ->(e) { handle_error(e) }
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,8 +9,6 @@ class ArticlesController < ApplicationController
   def show
     article = Article.find(params[:id])
     render json: serializer.new(article)
-  rescue ActiveRecord::RecordNotFound => e
-    render json: { message: e.message, detail: 'Here will be nicely formatted response' }
   end
 
   def serializer
